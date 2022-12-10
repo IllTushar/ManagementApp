f9a6a922f9bc3b9010371a5dbc8745546be3a30e
IllTushar <gtushar7668@gmail.com>
2022-12-11 00:07:52 +0530

    6th Commit

--- app/src/main/java/com/example/teachermanagement/Adapter/noticeRecyclerView.java
+++ app/src/main/java/com/example/teachermanagement/Adapter/noticeRecyclerView.java
@@ -6,13 +6,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.teachermanagement.DashBoard.Notice.CreateNotice;
+import com.example.teachermanagement.DashBoard.Notice.ViewNotice;
 import com.example.teachermanagement.Model.CreateNoticeModel;
 import com.example.teachermanagement.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class noticeRecyclerView extends FirebaseRecyclerAdapter<CreateNoticeModel,noticeRecyclerView.myViewHolder> {
     public noticeRecyclerView(@NonNull FirebaseRecyclerOptions<CreateNoticeModel> options) {
@@ -24,6 +29,15 @@ public class noticeRecyclerView extends FirebaseRecyclerAdapter<CreateNoticeMode
         holder.title.setText(model.getTitle());
         holder.date.setText(model.getDate());
       //  holder.link.setText(model.getNoticeUri());
+        holder.fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout,new ViewNotice(model.getTitle(),model.getDate(),model.getNoticeUri()))
+                        .addToBackStack(null).commit();
+            }
+        });
+
     }
 
     @NonNull
@@ -35,11 +49,14 @@ public class noticeRecyclerView extends FirebaseRecyclerAdapter<CreateNoticeMode
 
     public class myViewHolder extends RecyclerView.ViewHolder{
       TextView title,date,link;
+      FloatingActionButton fb;
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
             title  = itemView.findViewById(R.id.noticeTitle);
             date = itemView.findViewById(R.id.noticeDate);
            // link = itemView.findViewById(R.id.noticeLink);
+            fb = itemView.findViewById(R.id.btnViewNotice);
+
         }
     }
 }
